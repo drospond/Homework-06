fb85c39cc8ece8203b1e1a29c16b72aeef4d6349
drospond <david.rospond@gmail.com>
2020-03-16 21:28:52 -0400

    ajax call added to pull relevant weather info

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello world");
+$(document).ready(function(){
+    var cityName = "Atlanta";
+    var APIKey = "a012e3cf5aad2cc3ed0a6457c88685ce";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + cityName + "&appid=" + APIKey;
+    var latitude;
+    var longitude;
+    var queryURLUV = "api.openweathermap.org/data/2.5/uvi/history?lat=" + "&lon=" + "&appid=" + APIKey;
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        $("#cityName").text(response.name + " (" + moment().format("L") + ") " + response.weather[0].main);
+        $("#temperature").text(response.main.temp + "F");
+        $("#humidity").text(response.main.humidity + "%");
+        $("#wind-speed").text(response.wind.speed + " MPH");
+    })
+
+
+
+
+
+
+});
