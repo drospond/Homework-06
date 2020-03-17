28492352207ffeeab17b726561b7b3467a7023eb
drospond <david.rospond@gmail.com>
2020-03-16 22:08:14 -0400

    event listener added for history buttons

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 $(document).ready(function(){
+    var APIKey = "a012e3cf5aad2cc3ed0a6457c88685ce";
+    var latitude;
+    var longitude;
+    var queryURLUV = "api.openweathermap.org/data/2.5/uvi/history?lat=" + "&lon=" + "&appid=" + APIKey;
     
-
-    function search(event){
-        event.preventDefault();
-        var cityName = $("#search").val();
-        var APIKey = "a012e3cf5aad2cc3ed0a6457c88685ce";
+    function renderWeather(cityName){
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + cityName + "&appid=" + APIKey;
-        var latitude;
-        var longitude;
-        var queryURLUV = "api.openweathermap.org/data/2.5/uvi/history?lat=" + "&lon=" + "&appid=" + APIKey;
-    
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -22,9 +18,41 @@ $(document).ready(function(){
         })
     }
 
+    function search(event){
+        event.preventDefault();
+        var cityName = $("#search").val();
+        renderWeather(cityName);
+        addHistory(cityName);
+    }
+
+    function addHistory(cityName){
+        var historyButton = $("<button>");
+        historyButton.addClass("list-group-item");
+        historyButton.text(cityName);
+        $("#search-history").prepend(historyButton);
+    }
+
+    function historySearch(){
+        var cityName = $(this).text();
+        renderWeather(cityName);
+    }
+
     $("#search-button").on("click", search);
+    $("#search-history").on("click","button", historySearch)
 
 
 
 
 });
+
+/* <div class="card" style="width: 18rem;" id="five-day-forecast">
+              <div class="card-body">
+                <h5 class="card-title">Date</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Temperature:</h6>
+                <p class="card-text">Humdity:</p>
+              </div>
+            </div> */
+        //     var newCard = $("<div>");
+        // newCard.addClass("card-body");
+        // var h5 = $("<h5>");
+        // h5.addClass("card-title");
